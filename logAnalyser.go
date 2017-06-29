315e9ce72a748ccc117ac7bb7149daebde31035f
joshuabrudnak <joshuabrudnak@oakland.edu>
2017-06-29 14:02:40 -0400

    added columns to log data table

--- logAnalyser.go
+++ logAnalyser.go
@@ -17,12 +17,12 @@ import (
 const (
 	uMobileUses        = "create table if not exists uMobileUsers(id serial primary key, type text, ipAddress text, datetime text, requestType text, version text, protocol text, status int, time int)"
 	uMobileIpEntry     = "create table if not exists uMobileIpEntries(id serial primary key, type text, ipAddress text, numberOfUses text)"
-	uMobileData        = "create table if not exists uMobileLogData(id serial primary key, date text, iosLength int, androidLength int, duration text, iosTimeAverage int, androidTimeAverage int)"
+	uMobileData        = "create table if not exists uMobileLogData(id serial primary key, date text, startTime time, endTime time, iosLength int, androidLength int, duration text, iosTimeAverage int, androidTimeAverage int)"
 	clearUses          = "drop table uMobileUsers"
 	clearIpEntries     = "drop table uMobileIpEntries"
 	uMobileInsert      = "INSERT INTO uMobileUsers(type, ipAddress, datetime, requesttype, version, protocol, status, time) VALUES($1, $2, $3, $4, $5, $6, $7, $8)"
 	uMobileEntryInsert = "INSERT INTO uMobileIpEntries(type, ipAddress, numberofUses) VALUES($1, $2, $3)"
-	logDataInsert      = "INSERT INTO uMobileLogData(date, iosLength, androidLength, duration, iosTimeAverage, androidTimeAverage) VALUES($1, $2, $3, $4, $5, $6)"
+	logDataInsert      = "INSERT INTO uMobileLogData(date, startTime, endTime, iosLength, androidLength, duration, iosTimeAverage, androidTimeAverage) VALUES($1, $2, $3, $4, $5, $6, $7, $8)"
 )
 
 var db *sql.DB
@@ -35,26 +35,28 @@ type config struct {
 }
 
 type userInfo struct {
-    mobileType  string
+	mobileType  string
 	ipaddress   string
 	date        string
 	requestType string
-    version     string
+	version     string
 	protocol    string
 	status      int
 	time        int
 }
 
 type entryCount struct {
-	entry      string
-	number     int
+	entry  string
+	number int
 }
 
 type logData struct {
-	date     string
-	duration string
-	timeAvg  int
-	length   int
+	date      string
+    startTime string
+    endTime   string
+	duration  string
+	timeAvg   int
+	length    int
 }
 
 func checkErr(err error) {
@@ -83,8 +85,19 @@ func insertIpRow(sql string, finished chan bool, ipEntry entryCount, mType strin
 }
 
 func insertLogDataRow(sql string, androidInfo logData, iosInfo logData) {
-	_, err := db.Exec(sql, iosInfo.date, iosInfo.length, androidInfo.length, iosInfo.duration, iosInfo.timeAvg, androidInfo.timeAvg)
+	_, err := db.Exec(sql, iosInfo.date, iosInfo.startTime, iosInfo.endTime, iosInfo.length, androidInfo.length, iosInfo.duration, iosInfo.timeAvg, androidInfo.timeAvg)
 	checkErr(err)
+}
+
+func getTime(dateTime time.Time) string {
+	hour,min,sec := dateTime.Clock()
+    sHour := strconv.Itoa(hour)
+	sMin := strconv.Itoa(min)
+    sSec := strconv.Itoa(sec)
+    s := []string{sHour, ":", sMin, ":", sSec};
+    clock := strings.Join(s, "")
+
+	return clock
 }
 
 func grepLines(log *os.File, text string) []string {
@@ -133,7 +146,7 @@ func getEntryCount(entries []string) []entryCount {
 	return entryMembers
 }
 
-func analyseEntries(entries []userInfo, data chan logData) {
+func analyseEntries(entries []userInfo) logData {
 	var date []time.Time
 	var ipEntry []string
 	var statusEntry []string
@@ -177,6 +190,7 @@ func analyseEntries(entries []userInfo, data chan logData) {
 		timeAvg = 0
 	}
 
+	estart := time.Now()
 	for i := range entries {
 		go insertInfoRow(uMobileInsert, infoFinished[i], entries[i])
 	}
@@ -190,67 +204,55 @@ func analyseEntries(entries []userInfo, data chan logData) {
 		<-ipFinished[i]
 	}
 
+	elapsed := time.Since(estart)
+	fmt.Print("Query took ")
+	fmt.Println(elapsed)
+
+    //Set up log data struct
+	startTime, _ := time.Parse("02/Jan/2006:15:04:05 -0700", entries[0].date)
+    startClock := getTime(startTime)
+	endTime, _ := time.Parse("02/Jan/2006:15:04:05 -0700", entries[len(entries) - 1].date)
+    endClock := getTime(endTime)
 	year, month, day := date[0].Date()
 	sYear := strconv.Itoa(year)
 	sMonth := month.String()
 	sDay := strconv.Itoa(day)
-
 	logDate := fmt.Sprintf("%s/%s/%s", sMonth, sDay, sYear)
+	logStats := logData{logDate, startClock, endClock, difference.String(), timeAvg, entryLen}
 
-	logStats := logData{logDate, difference.String(), timeAvg, entryLen}
-
-	fmt.Print("Length: ")
-	fmt.Println(entryLen)
-	fmt.Print("Duration: ")
-	fmt.Println(difference)
-	fmt.Print("Time Average: ")
-	fmt.Println(timeAvg)
-
-	fmt.Println(len(data))
-	fmt.Println(logStats)
-	data <- logStats
+	return logStats
 }
 
 func getEntries(lines []string, mobileType string, data chan logData) {
 	entries := make([]userInfo, 0)
-	logStats := make(chan logData)
 
 	for i := 0; i < len(lines); i++ {
-        var versions string
+		var versions string
 		finalLine := trimString(lines[i])
 		lineParts := strings.Split(finalLine, " ")
-		time := 0
-
 		status, _ := strconv.Atoi(lineParts[6])
 		dateArray := []string{lineParts[1], lineParts[2]}
 		date := strings.Join(dateArray, " ")
+		time, _ := strconv.Atoi(lineParts[7])
 
-		if len(lineParts) > 7 {
-			time, _ = strconv.Atoi(lineParts[7])
-		}
-
-	    // find version number
+		// find version number
 		urlParts := strings.Split(lineParts[4], "/")
 		for j := range urlParts {
 			versionParts := strings.Split(urlParts[j], "")
 			if len(versionParts) > 0 {
-			    _, err := strconv.Atoi(versionParts[0])
+				_, err := strconv.Atoi(versionParts[0])
 				if err == nil {
 					versions = urlParts[j]
 					break
 				}
 			}
 		}
-
 		entry := userInfo{mobileType, lineParts[0], date, lineParts[3], versions, lineParts[5], status, time}
 		entries = append(entries, entry)
 	}
 
-	go analyseEntries(entries, logStats)
-	finalLogStat := <-logStats
-	fmt.Println("got stats")
-	fmt.Println(finalLogStat)
-	data <- finalLogStat
+	logStats := analyseEntries(entries)
+	data <- logStats
 }
 
 func getData(arg string, finished chan bool) {
@@ -270,11 +272,8 @@ func getData(arg string, finished chan bool) {
 		androidLines := grepLines(androidLog, "android")
 		go getEntries(iosLines, "iOS", iosLogData)
 		go getEntries(androidLines, "android", androidLogData)
-		fmt.Println("will get data")
 		iosData := <-iosLogData
 		androidData := <-androidLogData
-		fmt.Println("got data")
-
 		insertLogDataRow(logDataInsert, androidData, iosData)
 	}
 
@@ -312,7 +311,6 @@ func main() {
 		}
 		for i := range finished {
 			<-finished[i]
-			fmt.Println(i)
 		}
 	}
 	elapsed := time.Since(start)
